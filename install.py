4f23cbcdea4f20abc2559fdb406eb86e48680bda
bukan nama sebenarnya <antideface646@gmail.com>
2023-07-05 23:46:43 +0700

    install.py

--- install.py
+++ install.py
@@ -1,4 +1,3 @@
-import launch
 import os
 import pkg_resources
 import sys
@@ -24,31 +23,31 @@ if not os.path.exists(models_dir):
 if not os.path.exists(model_path):
     download(model_url, model_path)
 
-print("Checking roop requirements")
 with open(req_file) as file:
     for package in file:
         try:
-            python = sys.executable
             package = package.strip()
 
-            if not launch.is_installed(package):
-                print(f"Install {package}")
-                launch.run_pip(
-                    f"install {package}", f"sd-webui-roop requirement: {package}"
-                )
-            elif "==" in package:
-                package_name, package_version = package.split("==")
-                installed_version = pkg_resources.get_distribution(package_name).version
-                if installed_version != package_version:
-                    print(
-                        f"Install {package}, {installed_version} vs {package_version}"
-                    )
-                    launch.run_pip(
-                        f"install {package}",
-                        f"sd-webui-roop requirement: changing {package_name} version from {installed_version} to {package_version}",
-                    )
+            if "==" in package:
+                package_name, package_version = package.split('==')
+                try:
+                    installed_version = pkg_resources.get_distribution(package_name).version
+                    if installed_version != package_version:
+                        print(
+                            f"Running install of {package}, {installed_version} vs {package_version}"
+                        )
+                        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+                except pkg_resources.DistributionNotFound:
+                    print(f"Installing {package} as it is not found.")
+                    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+            else:
+                try:
+                    pkg_resources.get_distribution(package)
+                except pkg_resources.DistributionNotFound:
+                    print(f"Installing {package} as it is not found.")
+                    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
         except Exception as e:
             print(e)
-            print(f"Warning: Failed to install {package}, roop will not work.")
+            print(f"Warning: Failed to install {package}, nsfw-roop will not work.")
             raise e
